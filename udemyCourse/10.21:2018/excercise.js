b04a7a66594df2e05032ca93b32f978574083db4
stellarsteppe <42892144+stellarsteppe@users.noreply.github.com>
2018-10-22 04:07:52 -0500

    Add files via upload

---
+++ udemyCourse/10.21:2018/excercise.js
@@ -0,0 +1,10 @@
+/**
+ * Creator: steppe
+ * Date Created: 10/21/18
+ * Date Last Modified: 10.21.2018
+ */
+
+ $("div").css("backgroundColor", "purple");
+ $(".highlight").css("fontSize", "200px"); //or $(".hightlight").css("width", "200px");
+$("#third").css("border", "solid orange");
+$("div:first-of-type").css("color", "pink"); // or $("div:first").css("color", "pink"); jQuery, so its slower. the first code (running code) was a native css property so  it works faster
